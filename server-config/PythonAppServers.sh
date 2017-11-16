13a2cabb5a5609debd783499e91af37f45301970
marsmith <MARSMITH@USGS.GOV>
2017-11-16 09:40:13 -0500

    update server setup

--- server-config/PythonAppServers.sh
+++
@@ -1,4 +0,0 @@
-/usr/bin/nohup python /var/www/mapper/exporter/export.py > /dev/null 2>&1 &
-/usr/bin/nohup python /var/www/mapper/exporter/exportSM.py > /dev/null 2>&1 &
-/usr/bin/nohup python /var/www/mapper/exporter/siteCounter.py  > /dev/null 2>&1 &
-/usr/bin/nohup python /var/www/mapper/RGBFinder/getTileRGB.py > /dev/null 2>&1 &
